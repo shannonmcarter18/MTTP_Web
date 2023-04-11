2c172971e44631b94c8db4e85e2f6a494c4fd1c4
Tawba Wohid <tawbawohid@Tawbas-MacBook-Air.local>
2023-04-11 15:15:38 -0500

    exploring nuxt tutorials

---
+++ exploring_nuxt_3/pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<p>Product details for {{ id }}</p>
+<p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+    definePageMeta({
+        layout: 'products'
+    })
+</script>
+
+<style scoped>
+
+</style>
